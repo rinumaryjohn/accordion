c6b5b9f0efde20267f83f4def15bc38cb4e07c62
rinu-mary-john-89 <rinu89@gmail.com>
2026-02-12 08:27:27 +0300

    advanced

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,27 @@ import Question from "./Question";
 
 const App = () => {
   const [qst, setQst] = useState(questions);
+  const [activeId, setActiveId] = useState(null);
   console.log(qst);
+
+  const showOneQuestion = (id) => {
+    const newActiveId = id == activeId ? null : id;
+    setActiveId(newActiveId);
+  };
 
   return (
     <main>
       <section className="container">
         <h1>Questions</h1>
         {qst.map((obj) => {
-          return <Question key={obj.id} {...obj} />;
+          return (
+            <Question
+              key={obj.id}
+              {...obj}
+              activeId={activeId}
+              showOneQuestion={showOneQuestion}
+            />
+          );
         })}
       </section>
     </main>
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,22 +1,23 @@
 import React, { useState } from "react";
 import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 
-const Question = ({ title, info }) => {
-  const [toggle, setToggle] = useState(false);
-  const toggleQuestion = () => {
-    setToggle((currentValue) => {
-      return !currentValue;
-    });
-  };
+const Question = ({ id, title, info, activeId, showOneQuestion }) => {
+  // const [toggle, setToggle] = useState(false);
+  const isActive = id == activeId;
+  // const toggleQuestion = () => {
+  //   setToggle((currentValue) => {
+  //     return !currentValue;
+  //   });
+  // };
   return (
     <article className="question">
       <header>
         <h5>{title}</h5>
-        <button className="question-btn" onClick={toggleQuestion}>
-          {toggle ? <AiOutlineMinus /> : <AiOutlinePlus />}
+        <button className="question-btn" onClick={() => showOneQuestion(id)}>
+          {isActive ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {toggle && <p>{info}</p>}
+      {isActive && <p>{info}</p>}
     </article>
   );
 };
